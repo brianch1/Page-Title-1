4b99a501db82f1f7cb1371ad3dbc7700cd534e60
brianch1 <43443832+brianch1@users.noreply.github.com>
2019-01-03 09:50:11 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+$(".press").click(function() {
+    $(".cards").slideToggle();
+});
+
+$(".lol").click(function() {
+    $(".rain").hide();
+});
+
+$(".press").click(function() {
+    $(".pedro").slideToggle();
+});
